77b79d4fdc3cec2cc45baf2b0fb7b6a397f91b15
Vyshnav S Varma <vyshnavsvarma2255@gmail.com>
2024-08-05 22:58:24 +0530

    Added files

--- src/utils/utils.py
+++
@@ -1,7 +0,0 @@
-# Contains Common utilities
-# read data from the Excel file
-# read data from the csv,json file
-# set the headers - application/json, application/xml
-
-class Utils(object):
-    pass
